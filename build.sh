5281e205976da3fe8b0c3e54c93ef124c0ce82c5
David Robinson <drobinson@bluechiptechnology.com>
2022-10-04 13:27:40 +0100

    PI WIP

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
-make tm3linux_defconfig
+#make tm3linux_defconfig
 make -j8
 cp arch/arm64/boot/Image /tftpboot/
-cp ./arch/arm64/boot/dts/sunxi/tm3-hb5-7-c.dtb /tftpboot/tm3.dtb
+cp ./arch/arm64/boot/dts/sunxi/tm3-hbpicm4-cm4io.dtb /tftpboot/tm3.dtb
 
 cp arch/arm64/boot/Image ${TM3_LINUX_PACK_OUT_DIR}
 cp arch/arm64/boot/dts/sunxi/tm3*.dtb ${TM3_LINUX_PACK_OUT_DIR}
